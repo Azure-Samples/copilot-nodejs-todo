c7588ae1447e2a9ea0919befd7dbd1a495e1324c
sinedied <noda@free.fr>
2023-03-15 10:44:03 +0100

    feat: client/server integration

--- packages/client/src/environments/environment.ts
+++ packages/client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'https://func-todo-api-prod-pavmt75fjq3kk.azurewebsites.net/api'
+  api: 'https://localhost:3000/api'
 };
 
 /*
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,24 @@
+import path from 'node:path';
 import express from 'express';
 import pino from 'pino-http';
 import router from './routes/index';
 
+const publicDir = path.join(__dirname, '..', 'public');
 const app = express();
+
 app.use(pino());
+app.use(express.static(publicDir));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
+app.use('/api', router);
+
+// SPA fallback
+app.get('*', (req, res, next) => {
+  console.log(req.url);
+  if (req.url.startsWith('/api/')) {
+    return next();
+  }
+	res.sendFile(path.join(publicDir, 'index.html'));
+});
 
 export default app;
